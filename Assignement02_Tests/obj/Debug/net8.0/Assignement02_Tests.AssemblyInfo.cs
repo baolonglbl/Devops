f5b479b72365a74b40fba69feb53c77cb4a74491
baolonglbl <baolongco102@gmail.com>
2025-03-29 20:24:40 +0700

    thay doi lan n

--- Assignement02_Tests/obj/Debug/net8.0/Assignement02_Tests.AssemblyInfo.cs
+++ Assignement02_Tests/obj/Debug/net8.0/Assignement02_Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignement02_Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f72bed22fecf4c3274d96c47337c00a75b0c49ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignement02_Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignement02_Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
